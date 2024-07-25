59ab588afe90b141025ec408feeda8b2876134d6
simonazy <56880104+simonazy@users.noreply.github.com>
2024-07-25 07:40:22 -0700

    basics

---
+++ 01-Fundamentals-Part-1/03-let-const-var.js
@@ -0,0 +1,13 @@
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; //error
+// const job; //error
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
