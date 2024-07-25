3b3fd7cbae6278ffdb7234df84d865000bd2ba45
simonazy <56880104+simonazy@users.noreply.github.com>
2024-07-25 09:41:27 -0700

    basics part 1

---
+++ 01-Fundamentals-Part-1/11-equity-operator.js
@@ -0,0 +1,9 @@
+// Equality Operators: == vs. ===
+console.log(5 == "5");    // true
+console.log(5 === "5");   // false
+
+console.log(0 == false);  // true
+console.log(0 === false); // false
+
+console.log(null == undefined);  // true
+console.log(null === undefined); // false
