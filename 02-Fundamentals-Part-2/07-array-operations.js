46775596e7fd46e1c0ca6024373e0266f4cb428b
simonazy <56880104+simonazy@users.noreply.github.com>
2024-07-26 23:54:06 -0700

    part 2

---
+++ 02-Fundamentals-Part-2/07-array-operations.js
@@ -0,0 +1,33 @@
+// Basic Array Operations (Methods)
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength); 
+
+// Add to the front position
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+//Remove the first element
+friends.shift(); 
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+  }
